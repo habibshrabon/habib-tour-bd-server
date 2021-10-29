acd915a992c0a228fe22b84e326395072f59a59e
habibshrabon <snhabib777@gmail.com>
2021-10-29 17:32:48 +0600

    server start

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+//middelware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewzy7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from db it's working working");
+});
+
+app.listen(port, () => {
+  console.log("Running Genius server on port", port);
+});
