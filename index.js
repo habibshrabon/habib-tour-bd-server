44828d99592674088ba484c3990bd94a60bf7fbe
habibshrabon <snhabib777@gmail.com>
2021-10-29 22:36:31 +0600

    send data to database trying

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
-const ObjectId = require("mongodb").ObjectId;
-
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-//middelware
+//Middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewzy7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+//database connection
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("traveler");
+    const packageCollection = database.collection("packages");
+
+    //POST API
+    app.post("/packages", async (req, res) => {
+      const package = req.body;
+      console.log("Hit the post api", package);
+
+      const result = await packageCollection.insertOne(package);
+      console.log(result);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello from db it's working working");
